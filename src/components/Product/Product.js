16808733268c853501ae55a4e1152ebb74b63430
Akiram <akiram8112@gmail.com>
2025-05-02 07:35:02 -0800

    state -2

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,12 +6,19 @@ function Product() {
         cart: [],
         total: 0
     });
+    const currenyOptions = {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    }
+    const getTotal = () => {
+        return state.total.toLocaleString(undefined, currenyOptions);
+    }   
     return (
         <div className='wrapper'>
             <div>
                 Shopping Cart: {state.cart.length} total items
             </div>
-            <div>Total : {state.total}</div>
+            <div>Total : {getTotal()}</div>
             <div className='product'><span role='img' aria-label='ice-cream'></span></div>
             <button>Add</button><button>Remove</button>
         </div>

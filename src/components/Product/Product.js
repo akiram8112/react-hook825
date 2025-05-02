5932d9e206c27d3a3650bb9fd85a0b0f387cac72
Akiram <akiram8112@gmail.com>
2025-05-02 09:31:47 -0800

    array map function

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,6 +1,23 @@
 import React, { useState} from 'react';
 import './Product.css';
 
+const products = [
+    {
+     emoji: '🍦',
+     name: 'ice cream',
+     price: 5
+    },
+    {
+     emoji: '🍩',
+     name: 'donuts',
+     price: 2.5,
+    },
+    {
+     emoji: '🍉',
+     name: 'watermelon',
+     price: 4
+    }
+];
 function Product() {
     const [state, setState] = useState({
         cart: [],
@@ -13,10 +30,10 @@ function Product() {
     const getTotal = () => {
         return state.total.toLocaleString(undefined, currenyOptions);
     }
-    const add = () => {
+    const add = (product) => {
         setState({
-            cart: ['ice cream'],
-            total: 5
+            cart: [...state.cart, product.name],
+            total: state.total + product.price
         })
     }
     const remove = () => {
@@ -31,8 +48,14 @@ function Product() {
                 Shopping Cart: {state.cart.length} total items
             </div>
             <div>Total : {getTotal()}</div>
-            <div className='product'><span role='img' aria-label='ice-cream'></span></div>
-            <button onClick={ add }>Add</button><button>Remove</button>
+            <div>
+                {products.map((product)=>(
+                    <div key={product.name}>
+                        <div className='product'><span role='img' aria-label={product.name}>{product.emoji}</span></div>
+                        <button onClick={ add }>Add</button><button onClick={ remove }>Remove</button>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
